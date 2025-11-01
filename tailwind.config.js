3c80d258c2cf025d8a7e48288aed74e7112afd9e
aliciaab <alicia.5959.senac@gmail.com>
2025-11-01 09:54:16 -0300

    tentando

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js}",
+    "./frontend/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
