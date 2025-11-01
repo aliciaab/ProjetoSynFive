4b7884c86e57a5b9d04442f36a39cd0b1b5fa0d7
aliciaab <alicia.5959.senac@gmail.com>
2025-11-01 13:05:37 -0300

    nova tela infos

---
+++ js/Inform.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const irMapaBtn = document.getElementById("irMapa");
+
+  irMapaBtn.addEventListener("click", () => {
+    window.location.href = "./../html/Home.html"; 
+  });
+});
